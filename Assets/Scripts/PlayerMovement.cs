cdbdbbf93b4553f9bd3cafe39d94bdd340e04052
Mantis Coder <mantiscoder@gmail.com>
2025-08-01 20:00:54 -0700

    changed player into ball and added friction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float MoveSpeed = 6;
-    private float JumpHeight = 5;
+    private float MoveSpeed = 0.5f;
+    private float JumpHeight = 6;
     private float GravityScale = 1;
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,8 +22,9 @@ public class PlayerMovement : MonoBehaviour
     void CheckForInput()
     {
         float horizontalInput = Input.GetAxis("Horizontal"); // Uses Unity's Input Manager for A/D or Left/Right arrows
-        rb.linearVelocity = new Vector2(horizontalInput * MoveSpeed, rb.linearVelocityY);
-        if (Input.GetButtonDown("Jump"))
+        rb.AddForce(new Vector2(horizontalInput * MoveSpeed, 0));
+        //rb.linearVelocity = new Vector2(horizontalInput * MoveSpeed, rb.linearVelocityY);
+        if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, JumpHeight);
         }
