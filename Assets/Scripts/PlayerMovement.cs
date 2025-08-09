6fae15f833616da6f83f51c5299b9502593249d9
Mantis Coder <mantiscoder@gmail.com>
2025-08-08 19:49:49 -0700

    changed player physics, made ground wider, retextured player and ground, added Pew Pew Thing, changed movement method from velocity to angularvelocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float MoveSpeed = 1f;
-    private float JumpHeight = 6;
-    private float GravityScale = 1;
+    private float MoveSpeed = 3000f;
+    private float JumpHeight = 6f;
+    private float GravityScale = 1f;
     private bool IsGrounded;
+    private float maxAngularVelocity = 400f;
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,21 +20,29 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         CheckForInput();
+        rb.angularVelocity = Mathf.Clamp(rb.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
 
     }
 
     void CheckForInput()
     {
-        if (IsGrounded)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            float horizontalInput = Input.GetAxis("Horizontal"); // Uses Unity's Input Manager for A/D or Left/Right arrows
-            rb.AddForce(new Vector2(horizontalInput * MoveSpeed, 0));
-            //rb.linearVelocity = new Vector2(horizontalInput * MoveSpeed, rb.linearVelocityY);
-            if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocityX, JumpHeight);
-            }
+            rb.Sleep();
+            transform.position = new Vector2(0, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
         }
+        if (IsGrounded)
+            {
+                float horizontalInput = -1.0f * Input.GetAxis("Horizontal"); // Uses Unity's Input Manager for A/D or Left/Right arrows
+                rb.AddTorque(horizontalInput * MoveSpeed * Time.deltaTime, 0);
+                //rb.linearVelocity = new Vector2(horizontalInput * MoveSpeed, rb.linearVelocityY);
+                if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    rb.linearVelocity = new Vector2(rb.linearVelocityX, JumpHeight);
+                }
+            }
     }
 
     void OnCollisionStay2D(Collision2D collision)
